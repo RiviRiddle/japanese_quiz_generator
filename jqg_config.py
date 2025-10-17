3cbc6c26954fa8c1d12ae4021a0998f334439da3
RiviRiddle <42452135+RiviRiddle@users.noreply.github.com>
2025-10-17 14:57:02 -0600

    uploaded jqg_config (as I should've before)

---
+++ jqg_config.py
@@ -0,0 +1,7 @@
+# defaults
+
+description_default = False
+
+quiz_default = True
+
+japanese_display_default = 'romaji' # recommended default kana, this is just in case you can't figure out how to get the kana to work
